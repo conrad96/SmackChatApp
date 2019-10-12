7720b87147feee13090090355d4a45ecf75b72ca
conrad96 <conrad.mugisha@gmail.com>
2019-10-12 14:24:25 +0300

    fixed nav header components

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.smack
 
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -36,10 +37,16 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
-
-
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun loginBtnNavClicked(view: View){
+
+    }
+
+    fun addChannelBtn(view: View){
+
     }
 }
