7751e278d059a5f43d0bba87321107175871acc9
conrad96 <conrad.mugisha@gmail.com>
2019-11-02 17:10:22 +0300

    added code for displaying avatar images light/dark

--- app/src/main/java/com/example/smack/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -3,16 +3,33 @@ package com.example.smack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
+
 
 class CreateUserActivity : AppCompatActivity() {
 
+    var userAvatar = "profiledefault"
+    var avatorColor = "[0.5, 0.5, 0.5, 1]"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
-    fun generateUserAvatar(view: View){
 
+    fun generateUserAvatar(view: View){
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
+
+        if(color == 0){
+            userAvatar = "light$avatar"
+        }else{
+            userAvatar = "dark$avatar"
+        }
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceId)
     }
+
     fun generateColorClicked(view: View){
 
     }
